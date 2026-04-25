15919b25fc2487426532919de4e7f7540269e365
baronsmv <baronsmv@gmail.com>
2026-04-25 17:47:40 -0600

    Cleaned unused tile functions.

--- src/upscaler/tiles/__init__.py
+++ src/upscaler/tiles/__init__.py
@@ -3,13 +3,11 @@
 from .tile import TileProcessor
 from .utils import (
     expand_damage_rects,
-    extract_dirty_tiles_with_hash,
     extract_expanded_tiles,
 )
 
 __all__ = [
     "TileProcessor",
     "expand_damage_rects",
-    "extract_dirty_tiles_with_hash",
     "extract_expanded_tiles",
 ]
