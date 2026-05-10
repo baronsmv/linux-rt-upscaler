3d20ead1dc02c09225cc334f9724dc4d9406f173
baronsmv <baronsmv@gmail.com>
2026-05-09 19:42:13 -0600

    Creating script for Flatpak dependencies and add two new Flatpak build actions. We'll try when GUI release is near. For now, let's continue.

---
+++ scripts/generate_dependencies.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+
+# ===================================================================
+# Generate Flatpak dependency JSON
+# ===================================================================
+
+set root_dir (dirname (dirname (status --current-filename)))
+
+cd "$root_dir"
+
+uv export \
+    --format requirements-txt \
+    --no-editable \
+    | grep -v '^\.$' > requirements.txt
+
+pip install \
+    --dry-run \
+    --report python-dependencies.json \
+    -r requirements.txt \
+    --prefix /app
+
+rm requirements.txt
