494be9a22ae2faa126d5ec574990b21c42b5518b
baronsmv <baronsmv@gmail.com>
2026-05-16 13:28:01 -0600

    Updated script and .dektop.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 # Configuration
+APP_NAME="io.github.baronsmv.linux-rt-upscaler"
 DATA_URL="https://raw.githubusercontent.com/baronsmv/linux-rt-upscaler/refs/heads/main/data"
 DATA_DIR="$HOME/.local/share"
 
 DESKTOP_PATH="applications"
-DESKTOP_NAME="io.github.baronsmv.linux-rt-upscaler.desktop"
-DESKTOP_URL="$DATA_URL/$DESKTOP_PATH/$DESKTOP_NAME"
+DESKTOP_URL="$DATA_URL/$DESKTOP_PATH/$APP_NAME.desktop"
 DESKTOP_DIR="$DATA_DIR/$DESKTOP_PATH"
-DESKTOP_FILE="$DESKTOP_DIR/$DESKTOP_NAME"
+DESKTOP_FILE="$DESKTOP_DIR/$APP_NAME.desktop"
 
 ICON_PATH="icons/hicolor/256x256/apps"
-ICON_NAME="io.github.baronsmv.linux-rt-upscaler.png"
-ICON_URL="$DATA_URL/$ICON_PATH/$ICON_NAME"
+ICON_URL="$DATA_URL/$ICON_PATH/$APP_NAME.png"
 ICON_DIR="$DATA_DIR/$ICON_PATH"
-ICON_FILE="$ICON_DIR/$ICON_NAME"
+ICON_FILE="$ICON_DIR/$APP_NAME.png"
 
 # Binary location
 CMD_PATH="$HOME/.local/bin/upscale-gui"
 if command -v upscale-gui >/dev/null 2>&1; then
+    # Custom location
     CMD_PATH=$(command -v upscale-gui)
 fi
 if [ ! -x "$CMD_PATH" ]; then
@@ -36,13 +36,13 @@ echo "Downloading icon file..."
 curl -fsSL "$ICON_URL" -o "$ICON_FILE"
 chmod 644 "$ICON_FILE"
 
-# Desktop file (temp)
+# Desktop file
 echo "Downloading desktop file..."
+# Download to temp before edit
 TMP_DESKTOP=$(mktemp)
 curl -fsSL "$DESKTOP_URL" -o "$TMP_DESKTOP"
-# Replace placeholders
+# Replace binary placeholder
 sed -i "s|EXEC_PATH_PLACEHOLDER|$CMD_PATH|g" "$TMP_DESKTOP"
-sed -i "s|ICON_PATH_PLACEHOLDER|$ICON_FILE|g" "$TMP_DESKTOP"
 # Move temp
 mv "$TMP_DESKTOP" "$DESKTOP_FILE"
 chmod 644 "$DESKTOP_FILE"
