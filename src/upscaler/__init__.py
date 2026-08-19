aa8a46f24f1d6b4987b9d7e58ee64ea943cd78b4
baronsmv <baronsmv@gmail.com>
2026-08-19 00:45:41 -0600

    Add alias for API submodules.

--- src/upscaler/__init__.py
+++ src/upscaler/__init__.py
@@ -1,6 +1,11 @@
 """Real-Time Upscaler for Linux"""
 
+import sys
+
 from .api import UpscalerSession, exceptions, windows
+
+sys.modules.setdefault(__name__ + ".exceptions", exceptions)
+sys.modules.setdefault(__name__ + ".windows", windows)
 
 __all__ = [
     "UpscalerSession",
